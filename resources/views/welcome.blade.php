db7929fd4dc5fd930910486b9cdcb02566709e1a
feisalmombo <feisalmombo29@gmail.com>
2023-01-13 11:47:58 +0300

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,5 +13,34 @@
     <h1 class="text-3xl font-bold text-indigo-500 py-10 text-center">
         Hello 11 January 2023
     </h1>
+
+    <div class="container flex justify-center">
+        <table class="table-auto">
+            <thead>
+              <tr>
+                <th>Song</th>
+                <th>Artist</th>
+                <th>Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+              </tr>
+              <tr>
+                <td>Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+              </tr>
+              <tr>
+                <td>Shining Star</td>
+                <td>Earth, Wind, and Fire</td>
+                <td>1975</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
 </body>
 </html>
